e0a648690f07efccf628dab423bcca5b65477be3
Umair Shakeel <91723589+screamEagles@users.noreply.github.com>
2023-07-07 02:30:31 +0500

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,76 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAheQ7waqgtbjtA2cZcR7sEtpUSBq7BUUU",
+    authDomain: "rcaiform.firebaseapp.com",
+    databaseURL: "https://rcaiform-default-rtdb.firebaseio.com",
+    projectId: "rcaiform",
+    storageBucket: "rcaiform.appspot.com",
+    messagingSenderId: "129321012039",
+    appId: "1:129321012039:web:d1dfda29c7f5051919da62"
+};
+
+firebase.initializeApp(firebaseConfig);
+var rcaiFormDB = firebase.database().ref("rcaiForm");
+document.getElementById("rcaiForm").addEventListener("submit", submitForm);
+
+function submitForm(e) {
+    e.preventDefault();
+  
+    var name = getElementVal("name");
+    var emailid = getElementVal("emailid");
+    var msgContent = getElementVal("msgContent");
+  
+    saveMessages(name, emailid, msgContent);
+  
+    //   enable alert
+    document.querySelector(".alert").style.display = "block";
+  
+    //   remove the alert
+    setTimeout(() => {
+      document.querySelector(".alert").style.display = "none";
+    }, 3000);
+  
+    //   reset the form
+    document.getElementById("contactForm").reset();
+  }
+  
+  const saveMessages = (name, emailid, msgContent) => {
+    var newContactForm = contactFormDB.push();
+  
+    newContactForm.set({
+      name: name,
+      emailid: emailid,
+      msgContent: msgContent,
+    });
+  };
+  
+  const getElementVal = (id) => {
+    return document.getElementById(id).value;
+  };
+
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
